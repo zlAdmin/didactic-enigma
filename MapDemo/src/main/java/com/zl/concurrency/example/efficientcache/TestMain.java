4f7de33fdb8d783759f421a2c3b86ac2ee7b8fb3
zhang lei <13006315738@163.com>
2018-12-19 23:17:30 +0800

    维护代码

--- MapDemo/src/main/java/com/zl/concurrency/example/efficientcache/TestMain.java
+++ MapDemo/src/main/java/com/zl/concurrency/example/efficientcache/TestMain.java
@@ -4,10 +4,8 @@ import com.zl.concurrency.example.ThreadPoolResource.ThreadPoolInstance;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigInteger;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Semaphore;
 
 /**
  * @author zhagnlei
