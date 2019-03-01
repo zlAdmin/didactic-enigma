7785b1853ea11e7a75128bbf94dbe6fcbfb57322
zhanglei <zhanglei@acmtc.com>
2019-03-01 10:47:04 +0800

    新增测试工具

--- MapDemo/src/main/java/com/zl/demo/test/BigDecimalTest.java
+++ MapDemo/src/main/java/com/zl/demo/test/BigDecimalTest.java
@@ -1,12 +1,8 @@
 package com.zl.demo.test;
 
-import java.math.BigDecimal;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.NoSuchElementException;
 import java.util.Optional;
-
-import static com.zl.unit.printunit.PrintUnit.print;
 
 /**
  * sdfd
@@ -21,25 +17,10 @@ public class BigDecimalTest {
     private final static SimpleDateFormat formatter = new  SimpleDateFormat("yyyy-MM-dd");
 
     public static void main(String[] args) {
-        String time = "2014-10-11";
-        String time2 = "2014-10-13";
-        Optional<Object> planStartTime = Optional.ofNullable("2014-10-1");
-        Date timee = null;
-        Date  timee2 = null;
-        try {
-            timee = formatter.parse((String) planStartTime.get());
-            timee2 = formatter.parse(time2);
-            System.out.println(timee);
-            print(timee .before(timee2));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        String a = "3";
 
-        BigDecimal bigNum1 = new BigDecimal("10");
-        BigDecimal bigNum2 = new BigDecimal("5");
-
-        print(bigNum1.divide(bigNum2));
-
-
+        Optional.ofNullable(a).filter(item -> "1".equals(item) || "2".equals(item)).orElseThrow(
+                () -> new NoSuchElementException()
+        );
     }
 }
